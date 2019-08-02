ca20b48795016355298e605119a9945bde6ccde5
Harry Blackmore <harry.blackmore@annavalley.co.uk>
2019-08-02 13:27:09 +0100

    First push.
    Should work.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 Nova4KMonitoring
+
+Web page programmed to monitor up to 12 4K Novastar processors.
+
+The 4K Novastar processors have a built in web page GUI for monitoring over the network, but requires a page to be open for each one.
+
+This enables you to monitor 12 at any one time in one webpage when remote to the racks.
+
+I wouldnt try to control them though, this will probably break things, just monitor them!
+
+There is a setup.js file which enables you to label all of the processors, just open it and change the names.
+
+The IP addresses for the Novastars are pre-set, but could be changed if really required.
+
+They follow the following pattern:
+
+192.168.0.10
+192.168.0.11
+....
+192.168.0.21
+
+Let me know if you break it!
+
+Harry B
---
+++ setup.js
@@ -0,0 +1,28 @@
+var processor1Name = "4K - 1";
+var processor2Name = "4K - 2";
+var processor3Name = "4K - 3";
+var processor4Name = "4K - 4";
+var processor5Name = "4K - 5";
+var processor6Name = "4K - 6";
+var processor7Name = "4K - 7";
+var processor8Name = "4K - 8";
+var processor9Name = "4K - 9";
+var processor10Name = "4K - 10";
+var processor11Name = "4K - 11";
+var processor12Name = "4K - 12";
+
+function populate()
+{
+	document.getElementById("label1").innerHTML = processor1Name;
+	document.getElementById("label2").innerHTML = processor2Name;
+	document.getElementById("label3").innerHTML = processor3Name;
+	document.getElementById("label4").innerHTML = processor4Name;
+	document.getElementById("label5").innerHTML = processor5Name;
+	document.getElementById("label6").innerHTML = processor6Name;
+	document.getElementById("label7").innerHTML = processor7Name;
+	document.getElementById("label8").innerHTML = processor8Name;
+	document.getElementById("label9").innerHTML = processor9Name;
+	document.getElementById("label10").innerHTML = processor10Name;
+	document.getElementById("label11").innerHTML = processor11Name;
+	document.getElementById("label12").innerHTML = processor12Name;
+}
